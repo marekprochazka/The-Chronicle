3be665ffe0249070d7af89f74f5e33c9921d7bc1
Marek Procházka <m.prochazka2002@gmail.com>
2025-01-06 16:12:39 +0100

    linter

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <div>test</div>
     <NuxtWelcome />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  runtimeConfig: {},
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode',
+    'nuxt-twemoji',
+    '@nuxt/eslint',
+    '@pinia/nuxt',
+    'nuxt-easy-lightbox',
+    '@nuxt/image',
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
+  content: {
+    experimental: {
+      clientDB: true,
+    },
+  },
+  css: ['~/assets/styles/entry.scss'],
+  twemoji: {
+    expiresIn: 3.154e7, // SVG cache expiration time in seconds (1 year)
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      // failOnError: false,
+    },
+  },
+  ssr: false,
+});
