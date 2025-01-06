44997bf0808fca25f66f3dd2f01073933354458e
Marek Procházka <m.prochazka2002@gmail.com>
2025-01-06 16:20:45 +0100

    gallery file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
 
+## v0.0.1b
+
+[compare changes](https://github.com/marekprochazka/The-Chronicle/compare/v0.0.1...v0.0.1b)
+
 ## v0.0.1
 
---
+++ composables/_data/images.ts
@@ -0,0 +1,12 @@
+// This file is generated automatically by bin/update-gallery.py
+// Do not edit it manually
+// Last update: 2025-01-06 16:20:32.786273
+export type Img = {
+  src: string;
+  title: string;
+  alt: string;
+};
+
+export const images: Img[] = [
+    
+];
